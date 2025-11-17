8f9b1f3a85df2220fbae9ef93e8f63451a84801d
IFM <152510445+gitIliyaM@users.noreply.github.com>
2025-11-17 11:07:45 +0300

    Update AuditServiceJdbcTest.java

--- src/test/java/y_lab/service/AuditServiceJdbcTest.java
+++ src/test/java/y_lab/service/AuditServiceJdbcTest.java
@@ -14,7 +14,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
